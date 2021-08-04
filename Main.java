2ee111fe30a90444b798c71e6e01fc1169a37eaf
athulyaganesh <68687725+athulyaganesh@users.noreply.github.com>
2021-08-04 19:30:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    for(int i=0; i < 5; ++i){ //For loop feature 
+      System.out.println(i);
+    }
+    double test = 0.13564534656547657; //Double type feature 
+    System.out.println(test); 
+    int x = 5;
+    int y = 4;
+    int z;
+    z = x % y; //modulo feature 
+    System.out.println(z);
+  }
+}
---
+++ TokenType.java
@@ -0,0 +1,10 @@
+public enum TokenType {
+     Bool, Char, Double /*MODIFIED*/, Else, False, Float,
+     If, Int, Main, True, While, For /*MODIFIED*/, Void, Return, Print,
+     Eof, LeftBrace, RightBrace, LeftBracket, RightBracket,
+     LeftParen, RightParen, Semicolon, Comma, Assign,
+     Equals, Less, LessEqual, Greater, GreaterEqual,
+     Not, NotEqual, Plus, Minus, Multiply,
+     Divide, Modulo /*MODIFIED*/, And, Or, Identifier, IntLiteral,
+     FloatLiteral, CharLiteral, DoubleLiteral
+}
