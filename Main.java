af847d9e83b6a1aa0d015bc30a1b06cda24e6020
athulyaganesh <68687725+athulyaganesh@users.noreply.github.com>
2021-08-04 10:05:18 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 class Main {
+
   public static void main(String[] args) {
+      System.out.println("Testing the for loop feature");
     for(int i=0; i < 5; ++i){ //For loop feature 
       System.out.println(i);
     }
     double test = 0.13564534656547657; //Double type feature 
+    System.out.println("This is the double type: ");
     System.out.println(test); 
     int x = 5;
     int y = 4;
     int z;
     z = x % y; //modulo feature 
+    System.out.println("Result of 5 mod 4: ");
     System.out.println(z);
   }
 }
